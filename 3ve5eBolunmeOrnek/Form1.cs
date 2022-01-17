12033b5b04ebcc16037d3ab4ed9af84efd0b8103
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-17 18:21:55 +0300

    Proje dosyası ekle.

---
+++ 3ve5eBolunmeOrnek/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3ve5eBolunmeOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+
+            for (int i = sayi1; i <= sayi2; i++)
+            {
+                if (i%3==0 && i%5==0)
+                {
+                    listBox1.Items.Add(i.ToString());
+                }
+            }
+        }
+    }
+}
